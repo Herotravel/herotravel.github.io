bef9225ca33334035d528718dcf76736db5696ff
Herotravel <131613717+Herotravel@users.noreply.github.com>
2023-04-23 22:12:43 -0500

    TRIAL UPLOAD

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function changeLanguage(language) {
+	// Add logic to change content language based on selected language
+	if (language === 'es') {
+		// Change content to Spanish
+		alert('Content changed to Spanish');
+	} else {
+		// Change content to English
+		alert('Content changed to English');
+	}
+}
+``
